138fb5bf6094bc88a220f572adaebca9235e06bf
arfanovi <arfanovi.gub1@gmail.com>
2024-07-16 23:39:56 +0600

    html file read write method using

--- 03-Nodemon/index.js
+++ 03-Nodemon/index.js
@@ -1,36 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(
+const fs = require('fs')
+
+
+
+
+
+app.get("/page", (req, res) =>{
+    fs.readFile('./pages/index.html', (err, data) =>{
+        if(err){
+            console.log("Error", err);
+            res.send('<h1>Something went wrong</h1>')
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+})
+
+
+app.get('/home', (req, res) => {
+    res.send(     
         `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Node js Sweet</title>
-            <style>
-                div {
-                    background-color: aquamarine;
-                    color: black;
-                    text-align: center;
-                    box-sizing: border-box;
-                    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-                    height: 20vh;
-                    width: 20vh;
-                    padding: 2rem;
-                    margin: 10px;
-                }
-            </style>
-        </head>
-        <body>
-            <div>
-                <h1>Hello Node Js</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque placeat, explicabo totam voluptas cupiditate a! Ea animi ullam ipsa praesentium.</p>
-            </div>
-        </body>
-        </html>
+        <h1>Hello Home Route</h1>   
+        `
+    );
+});
+
+app.get('/about', (req, res) => {
+    res.send(     
+        `
+        <h1>Hello About Route</h1>   
+        `
+    );
+});
+
+
+app.get('/contact', (req, res) => {
+    res.send(     
+        `
+        <h1>Hello Contact Route</h1>   
         `
     );
 });
