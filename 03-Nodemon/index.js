2682e05adcb56962521bafe18a6b83cf0e1775da
arfanovi <arfanovi.gub1@gmail.com>
2024-07-16 20:46:20 +0600

    nodemon setup and adding code

--- 03-Nodemon/index.js
+++ 03-Nodemon/index.js
@@ -1,60 +1,40 @@
 const express = require('express');
 const app = express();
 
-
-app.get("/", (req, res) => {
-    res.send('Hello User');
-});
-
-// Like JSON 
-app.get("/json", (req, res) => {
-    res.json({
-        "name":"Ovi",
-    });
-} )
-
-
-// Express route to serve HTML content
-app.get("/htmlCode", (req, res) => {
-    res.send(`
+app.get('/', (req, res) => {
+    res.send(
+        `
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Styled Div Example</title>
-            <link rel="stylesheet" href="/styles.css">
+            <title>Node js Sweet</title>
             <style>
-                .styled-div {
+                div {
                     background-color: aquamarine;
                     color: black;
                     text-align: center;
                     box-sizing: border-box;
                     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-                
+                    height: 20vh;
+                    width: 20vh;
+                    padding: 2rem;
+                    margin: 10px;
                 }
             </style>
         </head>
         <body>
-            <div class="styled-div">
+            <div>
                 <h1>Hello Node Js</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque placeat, explicabo totam voluptas cupiditate a! Ea animi ullam ipsa praesentium.</p>
             </div>
         </body>
         </html>
-    `);
+        `
+    );
 });
 
-
-app.listen(4000, () => {
-    console.log("Server is Running Port is 4000")
-})
-
-
-
-function handlers(req, res, next) {
-    // read request object 
-    // process request 
-    // response back the result 
-    
-}
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
